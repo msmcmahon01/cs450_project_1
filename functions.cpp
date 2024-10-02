c29f965a8379f1cb60bc6dfff0aa55c595c70e05
Matthew McMahon <96601430+msmcmahon01@users.noreply.github.com>
2024-10-02 14:38:22 -0700

    Initial commit

---
+++ functions.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+#ifndef F_PI
+#define F_PI		((float)(M_PI))
+#define F_2_PI		((float)(2.f*F_PI))
+#define F_PI_2		((float)(F_PI/2.f))
+#endif
+
+void
+TrmptBell(int x_offset, int y_offset, int z_offset, int verticies, int tube_thinness) {
+
+}
+
+
